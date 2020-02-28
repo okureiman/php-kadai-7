3e9e19d269b33fb3134ff9747ac31f6ce08461a1
EC2 Default User <ec2-user@ip-172-31-32-35.us-east-2.compute.internal>
2020-02-28 05:54:48 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('news/create','Admin\NewsController@add');
+});
+
+// 課題1
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('news/create','Admin\AAAcontroller@BBB');
+});
+
+// 課題2
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('news/create','Admin\NewsController@add');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
